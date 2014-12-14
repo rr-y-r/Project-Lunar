edc7bd4fcbe2259a0d2442f9e76d6f15d346b948
keri wisnu nurhidayat <keriwisnu@gmail.com>
2014-12-14 22:59:41 +0700

    proggress php di controller dan model (server side) serta html di view (client side)

--- serverSide/application/config/config.php
+++ serverSide/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/projectLunar/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'rynaldoryan';
+$config['encryption_key'] = 'rynaldo';
 
 /*
 |--------------------------------------------------------------------------
