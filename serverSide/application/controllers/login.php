744a9e3fcb018ad359f41e830794552cb82b2aa8
alwanubaidillah <alwanubaidillah@gmail.com>
2014-12-14 22:50:39 +0700

    Progress HTML dan PHP

--- serverSide/application/controllers/login.php
+++ serverSide/application/controllers/login.php
@@ -1,15 +1,16 @@
 <?php
-class Login extends CI_controller{
+class Login extends CI_Controller {
     
-    function login(){
-        parent::construct();
-        $this->load->model('Mlogin','',TRUE);
-    }
+	function Login(){
+		parent::__construct();
+		$this->load->model('Login_model');
+	}
     
-    function index(){
-        $data       =   json_decode(file_get_contents('php://input'));
-        $dataCheck  =   $this->Mlogin->uCheck($data->uName,$data->pWord);
-        
-        echo json_encode($dataCheck);
-    }
+	 function index(){
+         $data    = json_decode(file_get_contents("php://input"));
+         $dataCheck = $this->Login_model->userCheck($data->username, $data->password);
+
+         echo json_encode($dataCheck);
+	 }
+
 }
